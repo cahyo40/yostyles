383934a85aa961f588b9d25dcf8b97de7640efa3
cahyo40 <mdcnugroho@gmail.com>
2023-02-11 07:33:11 +0700

    add font weight regular

--- lib/text.dart
+++ lib/text.dart
@@ -59,6 +59,7 @@ class YoFWeight {
   static const FontWeight extraLight = FontWeight.w200;
   static const FontWeight light = FontWeight.w300;
   static const FontWeight normal = FontWeight.w400;
+  static const FontWeight regular = FontWeight.w400;
   static const FontWeight medium = FontWeight.w500;
   static const FontWeight semiBold = FontWeight.w600;
   static const FontWeight bold = FontWeight.w700;
